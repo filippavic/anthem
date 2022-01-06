3f3033366e7cb3535ae0da4918af1dbe0f3073aa
Filip Pavić <filip.pavic@fer.hr>
2022-01-06 21:02:37 +0100

    Reverted the initial route

--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,7 @@ class MyApp extends StatelessWidget {
           unselectedItemColor: Colors.white38,
         ),
       ),
-      initialRoute: '/home',
+      initialRoute: '/',
       navigatorKey: _navigator,
       routes: <String, WidgetBuilder>{
         '/' : (context) => WelcomePage(),
